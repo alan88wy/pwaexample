6f8ce611ad701d5d2edf1d2ad267d0eb36812504
alan88wy <10415010+alan88wy@users.noreply.github.com>
2024-01-28 10:29:06 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example PWA App
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '9d30ba31cbeaad928537821c59771e23';
+
+export const fetchWeather = async (query) => {
+    // instead of getting the whole response, we only want the data part
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
+}
